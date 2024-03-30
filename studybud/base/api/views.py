1f6de2d6054f0e6f08f626d4dbb9c1dde52e031f
AyushModi123 <ayushonweb785@gmail.com>
2024-03-30 23:35:19 +0530

    add jwt token auth in api

--- studybud/base/api/views.py
+++ studybud/base/api/views.py
@@ -1,12 +1,17 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import authentication_classes, permission_classes
 from base.models import Room
 from .serializers import RoomSerializer
 from base.api import serializers
 
 
 @api_view(['GET'])
-def getRoutes(request):
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAuthenticated])
+def getRoutes(request):    
     routes = [
         'GET /api',
         'GET /api/rooms',
@@ -16,13 +21,17 @@ def getRoutes(request):
 
 
 @api_view(['GET'])
-def getRooms(request):
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAuthenticated])
+def getRooms(request):    
     rooms = Room.objects.all()
     serializer = RoomSerializer(rooms, many=True)
     return Response(serializer.data)
 
 
 @api_view(['GET'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAuthenticated])
 def getRoom(request, pk):
     room = Room.objects.get(id=pk)
     serializer = RoomSerializer(room, many=False)
